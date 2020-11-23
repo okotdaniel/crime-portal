03e7c2498cb796aaa8326b64d78fabd88d431f13
unknown <okotdanz@gmail.com>
2020-11-23 18:43:17 +0300

    initial commit

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
+from . import models 
 
 # Register your models here.
-
-
-from django.contrib import admin
-from . import models 
 
 admin.site.register(models.ReportSuspect)
 admin.site.register(models.wantedSuspect)
